b0890a9eaea3c38af4de2436f76e584af1ff99a8
Kássio San <kciosantana@gmail.com>
2025-07-20 22:16:50 -0300

    init project

---
+++ .github/instructions.md
@@ -0,0 +1,32 @@
+# About
+
+Todo app in Angular, that is used to learning TDD
+
+This app its is a simple app in Angular that have one component that implement all todo logic 
+
+# Stack
+- Angular 20
+- Tailwind
+- Vitest
+- Typescript
+
+# Features
+
+This app should have code smells, for example: 
+- Function with many reponsabilities, breaking the Single Resposability of Solid. 
+- Code duplicated breaking DRY - dont repeat yourself
+- Variables with bad names
+- Coupling with service
+- Dead code
+
+Create the code with issues and let comment behide it
+
+## Use cases
+- Create a new item
+- List items
+- Remove Item
+- Check item
+- Edit item
+- Save itens on service that saves in local storage
+
+
---
+++ example.ts
@@ -0,0 +1,32 @@
+
+
+
+describe("Deve criar um todo item", () => {
+    // Arrange
+    // initialization varaibles necessary for the test
+    const exampleVariable = "example";
+    
+    // Act
+    // call the function or method to be tested
+    const result = exampleFunction(exampleVariable);
+
+    // Assert
+    // check if the result is as expected
+    const expectedValue = "expectedValue";
+    expect(result).toBe(expectedValue);
+})
+
+describe("When [something happen] should [receive that]", () => {
+    // Arrange
+    // initialization varaibles necessary for the test
+    const requestInput = "example";
+    
+    // Act
+    // call the function or method to be tested
+    const response = doSomething(requestInput);
+
+    // Assert
+    // check if the response is as expected
+    const expectedValue = "expectedValue";
+    expect(response).toBe(expectedValue);
+})
